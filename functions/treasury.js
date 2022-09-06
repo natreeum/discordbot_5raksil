7c4c73035eb5d05e1820fe2b46ed355d43e78fde
Na_Treeum <projectkmscom@gmail.com>
2022-09-06 11:54:21 +0900

    update

--- functions/treasury.js
+++ functions/treasury.js
@@ -7,17 +7,23 @@ const BankManager = require(`../bank/BankManager`);
 const bankManager = new BankManager();
 
 async function getTreasury(interaction) {
+  await interaction.deferReply();
   let message = "";
   let i = 1;
   while (true) {
     const treasury = await getTreasuryData(i);
     if (!treasury) break;
     message += `id : ${treasury.id} | name : ${treasury.name} | amount : ${treasury.amount}\n`;
+    i++;
   }
-  await interaction.reply({ content: `${message}` });
+  if (i === 1) {
+    message = `등록된 트레져리가 없습니다.`;
+  }
+  await interaction.editReply({ content: `${message}` });
 }
 
 async function setTreasury(interaction, id, amount) {
+  await interaction.deferReply();
   const treasury = await getTreasuryData(id);
   if (!treasury) {
     await interaction.reply(`존재하지 않는 트레져리입니다.`);
@@ -27,7 +33,7 @@ async function setTreasury(interaction, id, amount) {
     amount: treasury.amount + amount,
   });
   await bankManager.depositBTC(interaction.user, amount);
-  await interaction.reply(
+  await interaction.editReply(
     `[${update.id}] ${update.name}의 수량이 ${update.amount} BTC 로 변경되었습니다.`
   );
 }
